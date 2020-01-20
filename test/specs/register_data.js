032bbcd6c79b589e244ff3436e7845e5f49ee487
Arthur0404 <predator777799@gmail.com>
2020-01-20 15:05:19 +0300

    new commit - URL Diary

--- test/specs/register_data.js
+++ test/specs/register_data.js
@@ -3,6 +3,7 @@ const HOST = 'https://stage.pasv.us';
 const URL_REGISTER = `${HOST}/user/register`;
 const URL_LOGIN = `${HOST}/user/login`;
 const URL_ADMIN_LOGIN = `${HOST}/diary`;
+const URL_DIARY = `${HOST}/diary/create`;
 
 const email = Math.random()+ 'ara8977@gmail.com';
 const user = {
